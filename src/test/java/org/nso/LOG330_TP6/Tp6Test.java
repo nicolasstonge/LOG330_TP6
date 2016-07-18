44fd3f37468ba58e409e8d1cb97022fa859ba47d
Nicolas St-Onge <stongenicolas@hotmail.com>
2016-07-18 16:55:24 -0400

    add tes 2 and 3

--- src/test/java/org/nso/LOG330_TP6/Tp6Test.java
+++ src/test/java/org/nso/LOG330_TP6/Tp6Test.java
@@ -17,9 +17,36 @@ public class Tp6Test {
 		_calculateur = new CalculVariance("src/main/java/org/nso/LOG330_TP6/numbers.csv");
 	}
 	
+	@BeforeClass
+	public static void initialise2(){
+
+		_calculateur = new CalculVariance("src/main/java/org/nso/LOG330_TP6/numbers2.csv");
+	}
+	
+	@BeforeClass
+	public static void initialise3(){
+
+		_calculateur = new CalculVariance("src/main/java/org/nso/LOG330_TP6/numbers3.csv");
+	}
+	
 	@Test
-	public void firstTest(){
+	public void testHighVar(){
 		
+		initialise();
 		Assert.assertEquals(391417.88, _calculateur.CalculVar(), 0.2);
+	}
+	
+	@Test
+	public void testLowVar(){
+		
+		initialise2();
+		Assert.assertEquals(9.166666984558105, _calculateur.CalculVar(), 0.2);
+	}
+	
+	@Test
+	public void testNullVar(){
+		
+		initialise3();
+		Assert.assertEquals(0, _calculateur.CalculVar(), 0.2);
 	}
 }
