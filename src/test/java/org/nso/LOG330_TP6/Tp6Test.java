06755e35fc5f721393f558aed19260738540f097
Nicolas St-Onge <stongenicolas@hotmail.com>
2016-07-18 14:23:47 -0400

    add test 2 and 3

--- src/test/java/org/nso/LOG330_TP6/Tp6Test.java
+++ src/test/java/org/nso/LOG330_TP6/Tp6Test.java
@@ -18,9 +18,38 @@ public class Tp6Test {
 		_calculateur = new CalculInterval("src/main/java/org/nso/LOG330_TP6/donnees.csv");
 	}
 	
+	@BeforeClass
+	public static void initialise2(){
+
+		// Instance de la classe de calcul de regression lineaire
+		_calculateur = new CalculInterval("src/main/java/org/nso/LOG330_TP6/donnees2.csv");
+	}
+	
+	@BeforeClass
+	public static void initialise3(){
+
+		// Instance de la classe de calcul de regression lineaire
+		_calculateur = new CalculInterval("src/main/java/org/nso/LOG330_TP6/donnees3.csv");
+	}
+	
 	@Test
-	public void firstTest(){
+	public void testAt80(){
 		
-		Assert.assertEquals(92.39868f, _calculateur.calculIntervalle(1.250f, "70"), 0.002);
+		initialise();
+		Assert.assertEquals(121.07923889160156f, _calculateur.calculIntervalle(1.638f, "80"), 0.002);
+	}
+	
+	@Test
+	public void testAt70(){
+		
+		initialise2();
+		Assert.assertEquals(76.71610260009766f, _calculateur.calculIntervalle(1.250f, "70"), 0.002);
+	}
+	
+	@Test
+	public void testAt90(){
+		
+		initialise3();
+		Assert.assertEquals(135.93719482421875f, _calculateur.calculIntervalle(2.353f, "90"), 0.002);
 	}
 }
