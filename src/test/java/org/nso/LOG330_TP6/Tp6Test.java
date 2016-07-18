8bc17ab53d325dec18d18c2a583c9ec0a9774fb5
Nicolas St-Onge <stongenicolas@hotmail.com>
2016-07-18 13:27:44 -0400

    add test 2 and 3

--- src/test/java/org/nso/LOG330_TP6/Tp6Test.java
+++ src/test/java/org/nso/LOG330_TP6/Tp6Test.java
@@ -5,6 +5,8 @@ package org.nso.LOG330_TP6;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
+import org.junit.runner.notification.Failure;
 import org.nso.LOG330_TP6.CalculEffort;
 
 public class Tp6Test {
@@ -18,9 +20,44 @@ public class Tp6Test {
 		_calculateur = new CalculEffort("src/main/java/org/nso/LOG330_TP6/donnees.csv");
 	}
 	
+	@BeforeClass
+	public static void initialise2(){
+
+		// Instance de la classe de calcul de regression lineaire
+		_calculateur = new CalculEffort("src/main/java/org/nso/LOG330_TP6/donnees2.csv");
+	}
+	
+	@BeforeClass
+	public static void initialise3(){
+
+		// Instance de la classe de calcul de regression lineaire
+		_calculateur = new CalculEffort("src/main/java/org/nso/LOG330_TP6/donnees3.csv");
+	}
+	
 	@Test
-	public void firstTest(){
+	public void testLowCorrelation(){
 		
+		initialise();
 		Assert.assertEquals(0.1552162305176995, _calculateur.calculCorrelation(), 0.002);
+	}
+	
+	@Test
+	public void testHighCorrelation(){
+		
+		initialise2();
+		Assert.assertEquals(1.0, _calculateur.calculCorrelation(), 0.002);
+	}
+	
+	@Test
+	public void testInvalid() throws Exception{
+		
+		initialise3();
+		if(Double.isNaN(_calculateur.calculCorrelation())){
+			
+		}else{
+			Exception e = null;
+			throw e;
+		}
+		
 	}
 }
